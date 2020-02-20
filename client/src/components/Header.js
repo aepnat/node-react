e518d149d00b0fd52eb7602b77251e99e753dd94
aepnat <aep.nat@gmail.com>
2020-02-20 16:11:14 +0700

    init react client

---
+++ client/src/components/Header.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class Header extends Component {
+    renderContent() {
+        switch (this.props.auth) {
+            case null:
+                return '';
+            case false:
+                return <li><a href="/auth/google">Login with Google</a></li>;
+            default:
+                return <li><a href="/logout">Logout</a></li>;
+        }
+    }
+
+    render() {
+        return(
+            <nav>
+                <div className="nav-wrapper">
+                    <a href="/" className="left brand-logo">Node React</a>
+                    <ul id="nav-mobile" className="right">
+                        {this.renderContent()}
+                    </ul>
+                </div>
+            </nav>
+        );
+    }
+}
+
+function mapStatetoProps({ auth }) {
+    return { auth };
+}
+
+export default connect(mapStatetoProps)(Header);
