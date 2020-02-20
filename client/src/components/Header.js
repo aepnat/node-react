7c0272f5f14099ba298b35ad9439471da23b6829
aepnat <aep.nat@gmail.com>
2020-02-20 16:20:03 +0700

    logo Link

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 class Header extends Component {
     renderContent() {
@@ -17,7 +18,7 @@ class Header extends Component {
         return(
             <nav>
                 <div className="nav-wrapper">
-                    <a href="/" className="left brand-logo">Node React</a>
+                    <Link to={this.props.auth ? '/surveys' : '/'} className="left brand-logo">Node React</Link>
                     <ul id="nav-mobile" className="right">
                         {this.renderContent()}
                     </ul>
